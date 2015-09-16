598c3fede224b31cd4c0f816df7f2a92b4afb4fd
Mike Koss <koss@google.com>
2015-09-15 22:46:55 -0700

    Simulation no longer relies on custom token - use auth.uid.

--- test/mail-test.js
+++ test/mail-test.js
@@ -18,16 +18,18 @@ var rulesSuite = bolt.rulesSuite;
 var secrets = require('./auth-secrets');
 
 rulesSuite("Mail", function(test) {
+  var uid = test.uid;
+
   test.database(secrets.APP, secrets.SECRET);
   test.rules('test/samples/mail');
 
   test("Inbox tests.", function(rules) {
     rules
       .as('tom')
-      .at('/users/bill/inbox/1')
+      .at('/users/' + uid('bill') + '/inbox/1')
       .write({
-        from: 'tom',
-        to: 'bill',
+        from: uid('tom'),
+        to: uid('bill'),
         message: 'Hi, Bill!'
       })
       .succeeds("Normal write.")
@@ -36,38 +38,38 @@ rulesSuite("Mail", function(test) {
       .fails("Sender cannot delete sent message.")
 
       .write({
-        from: 'tom',
-        to: 'bill',
+        from: uid('tom'),
+        to: uid('bill'),
         message: 'Hello, again!'
       })
       .fails("Sender cannot overwrite.")
 
-      .at('/users/bill/inbox/2')
+      .at('/users/' + uid('bill') + '/inbox/2')
       .write({
-        from: 'tom',
-        to: 'bill',
+        from: uid('tom'),
+        to: uid('bill'),
         message: 'Hi, Bill!',
         spurious: 'supurious data'
       })
       .fails("No undefined fields.")
 
       .write({
-        from: 'george',
-        to: 'bill',
+        from: uid('george'),
+        to: uid('bill'),
         message: 'Hi, Bill!'
       })
       .fails("From field should be correct.")
 
-      .at('/users/bill/inbox/1/message')
+      .at('/users/' + uid('bill') + '/inbox/1/message')
       .write("Bill gets my inheritance")
       .fails("Cannnot tamper with message.")
 
-      .at('/users/bill/inbox/1/from')
-      .write('bill')
+      .at('/users/' + uid('bill') + '/inbox/1/from')
+      .write(uid('bill'))
       .fails("Cannot tamper with from field.")
 
       .as('bill')
-      .at('/users/bill/inbox/1')
+      .at('/users/' + uid('bill') + '/inbox/1')
       .write(null)
       .succeeds("Receiver can delete received mail.");
   });
@@ -76,10 +78,10 @@ rulesSuite("Mail", function(test) {
   test("Outbox tests.", function(rules) {
     rules
       .as('bill')
-      .at('/users/bill/outbox/1')
+      .at('/users/' + uid('bill') + '/outbox/1')
       .write({
-        from: 'bill',
-        to: 'tom',
+        from: uid('bill'),
+        to: uid('tom'),
         message: "Hi, Tom!"
       })
       .succeeds("Normal write.")
@@ -90,16 +92,16 @@ rulesSuite("Mail", function(test) {
 
       .as('bill')
 
-      .at('/users/bill/outbox/1/message')
+      .at('/users/' + uid('bill') + '/outbox/1/message')
       .write("Bill gets my inheritance.")
       .fails("Sender cannot tamper with outbox message.")
 
-      .at('/users/bill/outbox/1/from')
+      .at('/users/' + uid('bill') + '/outbox/1/from')
       .write('bill')
       .fails("Can't do a partial overwrite - even if same data.")
 
       .as('bill')
-      .at('/users/bill/outbox/2')
+      .at('/users/' + uid('bill') + '/outbox/2')
       .write({
         from: 'joe',
         to: 'tom',
@@ -115,7 +117,7 @@ rulesSuite("Mail", function(test) {
       })
       .fails("No undefined fields.")
 
-      .at('/users/bill/outbox/1')
+      .at('/users/' + uid('bill') + '/outbox/1')
       .write(null)
       .succeeds("Sender can delete sent mail in outbox.");
   });
@@ -123,37 +125,37 @@ rulesSuite("Mail", function(test) {
   test("Read permissions.", function(rules) {
     rules
       .as('bill')
-      .at('/users/bill/outbox/1')
+      .at('/users/' + uid('bill') + '/outbox/1')
       .write({
-        from: 'bill',
-        to: 'tom',
+        from: uid('bill'),
+        to: uid('tom'),
         message: 'Hi, Tom!'
       })
       .succeeds("Normal write.")
 
       .as('tom')
-      .at('/users/bill/inbox/1')
+      .at('/users/' + uid('bill') + '/inbox/1')
       .write({
-        from: 'tom',
-        to: 'bill',
+        from: uid('tom'),
+        to: uid('bill'),
         message: 'Hi, Bill!'
       })
 
       .as('bill')
-      .at('/users/bill/inbox/1')
+      .at('/users/' + uid('bill') + '/inbox/1')
       .read()
       .succeeds("Can read own inbox.")
 
-      .at('/users/bill/outbox/1')
+      .at('/users/' + uid('bill') + '/outbox/1')
       .read()
       .succeeds("Can read own outbox.")
 
       .as('tom')
-      .at('/users/bill/inbox/1')
+      .at('/users/' + uid('bill') + '/inbox/1')
       .read()
       .fails("Can't read Bill's inbox.")
 
-      .at('/users/bill/outbox/1')
+      .at('/users/' + uid('bill') + '/outbox/1')
       .read()
       .fails("Can't read Bills outbox.");
   });
