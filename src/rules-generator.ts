cee609de062ef6d9b146e52aabe67b2fbdba444b
Mike Koss <koss@google.com>
2015-09-17 14:54:23 -0700

    need require def

--- src/rules-generator.ts
+++ src/rules-generator.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/// <reference path="../typings/node.d.ts" />
 var util = require('./util');
 var ast = require('./ast');
 
