6759f75d1ae31d4913074262a5eaadc33f86ec87
Mike Koss <koss@google.com>
2016-08-01 18:16:54 -0700

    polyfill Promise for node 0.10

--- src/bolt.ts
+++ src/bolt.ts
@@ -13,6 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// TODO(koss): After node 0.10 leaves LTS - remove polyfilled Promise library.
+if (typeof Promise === 'undefined') {
+  require('es6-promise').polyfill();
+}
+
 let parser = require('./rules-parser');
 import * as generator from './rules-generator';
 import * as astImport from './ast';
